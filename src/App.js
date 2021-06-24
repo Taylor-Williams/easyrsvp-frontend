62b3b539a2538ecf2c0691352a3f83c4d8afdb2a
Taylor-Williams <taylor@thewilliams.net>
2021-06-24 16:02:41 -0700

    importing redux/react components and testing connection to backend

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React from 'react'
 
 class App extends React.Component {
+
+  componentDidMount() {
+    fetch('http://localhost:3000/api/v1/venues')
+    .then(response => response.json())
+    .then(data => console.log(data))
+  }
   render() {
     return (
       <div className='App'> 
